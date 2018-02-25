8f85dea8153c9268907506acbe7f2f151353067b
Cory Brown <Cory.Brown@sbcs.com>
2018-02-24 23:51:22 -0500

    Minor edits

--- Assets/HopBlock2/Scripts/Player.cs
+++ Assets/HopBlock2/Scripts/Player.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
 
     Rigidbody2D rb;
@@ -14,25 +15,25 @@ public class Player : MonoBehaviour {
 
     bool onGround;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
         onGround = Physics2D.OverlapCircle(groundCheck.position,
             groundCheckRadius, groundIsWhere);
 
         rb.velocity = new Vector2(3, rb.velocity.y);
 
-        if(Input.GetMouseButton(0) && onGround)
+        if (Input.GetMouseButton(0) && onGround)
         {
             rb.velocity = new Vector2(rb.velocity.x, 5);
         }
-	}
+    }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
@@ -54,4 +55,5 @@ public class Player : MonoBehaviour {
             SceneManager.LoadScene(2);
         }
     }
+
 }
