c4431c57774946f8af86a0517c5c5275c310886f
Java-Johnrh <35967122+Java-Johnrh@users.noreply.github.com>
2018-07-21 16:18:11 -0400

    Two player chaos mode
    That's what I'm calling this. Because at the moment, it's overly simple. The game is two screens as Timm wanted,  but there is only one player displayed on both screens. Both screens can be bumped to cause the player to jump, but with two players trying to control the jumping at once it would be chaos.
    
    NOT A REAL OR GOOD GAME MODE.
    Dev game mode really.

--- Assets/02-Scripts/GOD.cs
+++ Assets/02-Scripts/GOD.cs
@@ -1,7 +1,15 @@
 ﻿using UnityEngine;
 
 public class GOD : MonoBehaviour {
+    public GameObject player1LevelAssembler;
+    public GameObject player2LevelAssembler;
+    public bool twoPlayer = false;
     //load in score object
     //score addition over time
     //collect item for money
+
+    private void Start()
+    {
+        
+    }
 }
